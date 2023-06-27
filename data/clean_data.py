ef50d8b3d093b30bba1604bf5cd3c05f0371719b
DanielZlln <daniel_zellner@icloud.com>
2023-06-27 13:23:56 +0200

    new feiertage nrw data

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,10 +1,10 @@
-import pandas as pd
 from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
 from datetime import datetime, timedelta, date
+import pandas as pd
 from pathlib import Path
 import os
 from os import walk
-from dateutil.parser import parse
 import locale
 
 def get_src_directory(script_directory):
@@ -30,30 +30,31 @@ def clean_neutor_excel(file_name):
     
     df_neutor = pd.concat(neutor_xlsx.values(), ignore_index=True)
     
-    rename_cols = {
+    new_column_names = {
                         'Neutor': 'Neutor (gesamt)',
                         'FR stadteinwärts': 'Neutor FR stadteinwärts',
                         'FR stadtauswärts': 'Neutor FR stadtauswärts',
+                        'Time' : 'Zeit'
                     }
-    df_neutor = df_neutor.astype(str).rename(columns=rename_cols)
-    
+    df_neutor = df_neutor.rename(columns=new_column_names)
     
     if 'Gefühlte Temperatur (°C)' in df_neutor.columns:
         df_neutor = df_neutor.drop('Gefühlte Temperatur (°C)', axis=1)
         
     if 'Unnamed: 0' in df_neutor.columns:
         df_neutor = df_neutor.drop(columns='Unnamed: 0', axis=1)
-        
+
     if 'Zeit' in df_neutor.columns:
         success_date = False
-        success_cols = False
-        
+
         try:
             df_neutor['Datum'] = pd.to_datetime(df_neutor['Zeit'], format='%Y-%m-%d').dt.date.astype(str)
             df_neutor['Zeit'] = pd.to_datetime(df_neutor['Zeit']).dt.time
+            
             success_date = True
         except ValueError:
             print('Konnte nicht in das Foramt bereinigt werden')
+        
         if not success_date:
             try:
                 df_neutor['Zeit'] = df_neutor['Zeit'].astype(str)
@@ -78,11 +79,7 @@ def clean_neutor_excel(file_name):
             try:
                 df_neutor = df_neutor[['Datum', 'Zeit', 'Neutor (gesamt)', 'Neutor FR stadteinwärts', 'Neutor FR stadtauswärts', 'Wetter', 'Temperatur (°C)', 
                             'Luftfeuchtigkeit (%)', 'Regen (mm)', 'Wind (km/h)']]
-                #success_cols = True
             except (ValueError, KeyError):
                 print('Falsche Spaltenbezeichnung')
+            
         return df_neutor
-
-df_neutor_18 = clean_neutor_excel('Zaehlstelle_Neutor_2018_Stundenauswertung.xlsx')
-
-print(df_neutor_18)
